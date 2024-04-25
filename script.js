c3066e2cb3cb989eb1d896671dfca8c520eb6f64
Roman <pomincukr@gmail.com>
2024-04-25 19:59:32 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number){
+    if(number % 2 === 0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+      }
+}
+console.log(evenOrOdd(2))
+console.log(evenOrOdd(1))
+console.log(evenOrOdd(-42))
